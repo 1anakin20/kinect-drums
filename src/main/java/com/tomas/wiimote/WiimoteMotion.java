2bb36f4ba8b82e006328df5507cd88afb796e609
Tomas Daniel Nieto <tomasdaniel09@hotmail.com>
2021-06-28 10:11:47 -0400

    zForce field on WiimoteMotion

--- src/main/java/com/tomas/wiimote/WiimoteMotion.java
+++ src/main/java/com/tomas/wiimote/WiimoteMotion.java
@@ -6,9 +6,14 @@ import wiiusej.values.RawAcceleration;
 import wiiusej.wiiusejevents.physicalevents.MotionSensingEvent;
 
 public class WiimoteMotion extends WiimoteEventsAdapter {
+	private float zForce;
 
 	@Override
 	public void onMotionSensingEvent(MotionSensingEvent motionSensingEvent) {
+		zForce = motionSensingEvent.getGforce().getZ();
+	}
 
+	public float getzForce() {
+		return zForce;
 	}
 }
