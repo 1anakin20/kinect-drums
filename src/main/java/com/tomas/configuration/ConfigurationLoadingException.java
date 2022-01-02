bc29974b915eda0d0736bf37edc29c98e6e330f1
Tomas Daniel Nieto <tomasdaniel09@hotmail.com>
2022-01-02 14:20:57 -0500

    ConfigurationLoadingException javadocs

--- src/main/java/com/tomas/configuration/ConfigurationLoadingException.java
+++ src/main/java/com/tomas/configuration/ConfigurationLoadingException.java
@@ -1,5 +1,8 @@
 package com.tomas.configuration;
 
+/**
+ * When the configuration could not have been loaded
+ */
 public class ConfigurationLoadingException extends RuntimeException {
     public ConfigurationLoadingException(String message) {
         super(message);
