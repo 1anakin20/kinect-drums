40f1aa60aaea5025581d6783f11a96cc7d940ce0
Tomas Daniel Nieto <tomasdaniel09@hotmail.com>
2022-01-02 14:22:18 -0500

    ConfigurationSavingException javadocs

--- src/main/java/com/tomas/configuration/ConfigurationSavingException.java
+++ src/main/java/com/tomas/configuration/ConfigurationSavingException.java
@@ -1,5 +1,8 @@
 package com.tomas.configuration;
 
+/**
+ * When the configuration could not be saved
+ */
 public class ConfigurationSavingException extends RuntimeException {
     public ConfigurationSavingException(String message) {
         super(message);
