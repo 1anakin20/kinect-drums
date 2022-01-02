89d90a63e00c33547d15390f34e4890b13a04c84
Tomas Daniel Nieto <tomasdaniel09@hotmail.com>
2022-01-02 14:20:01 -0500

    Configuration javadocs

--- src/main/java/com/tomas/configuration/Configuration.java
+++ src/main/java/com/tomas/configuration/Configuration.java
@@ -6,17 +6,34 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Properties;
 
+/**
+ * Configuration of the app
+ */
 public class Configuration {
     private final Properties properties;
 
+    /**
+     * Constructor
+     * @param properties Loaded properties
+     */
     protected Configuration(Properties properties) {
         this.properties = properties;
     }
 
-    public String getValue(ConfigurationKeys value) {
-        return properties.getProperty(value.getValue());
+    /**
+     * Gets a configuration value
+     * @param key {@link ConfigurationKeys} configuration key
+     * @return value of the configuration
+     */
+    public String getValue(ConfigurationKeys key) {
+        return properties.getProperty(key.getValue());
     }
 
+    /**
+     * Sets a configuration to a value
+     * @param key {@link ConfigurationKeys} key to change value
+     * @param value new value
+     */
     public void setValue(ConfigurationKeys key, String value) {
         properties.setProperty(key.getValue(), value);
 
