309a9eb11563fe060c7bf02c80ed8a1fa7dcdd26
Tomas Daniel Nieto <tomasdaniel09@hotmail.com>
2022-01-02 19:47:37 -0500

    Load configuration at the start

--- src/main/java/com/tomas/KinectDrumsApp.java
+++ src/main/java/com/tomas/KinectDrumsApp.java
@@ -1,7 +1,10 @@
 package com.tomas;
 
+import com.tomas.configuration.ConfigurationLoader;
+
 public class KinectDrumsApp {
 	public static void main(String[] args) {
+		ConfigurationLoader.loadConfiguration();
 		Game game = new Game();
 		game.start();
 	}
